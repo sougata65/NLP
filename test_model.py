6992da7b806c0aa7228d383fa42de85562b81482
Sougata Bhattacharya <sougata.b@zohocorp.com>
2019-06-20 17:20:26 +0530

    commit# 8785

--- test_model.py
+++ test_model.py
@@ -1,15 +1,17 @@
 from sklearn.externals import joblib
+import Sentiment\ Analysis\
 import os
 import sys
 current_path = os.getcwd()
 args = sys.argv
 
 data = args[0]
-model = None
-try:
-    model = joblib.load(current_path+"/Models/sentiment_analysis_naive_bayes_model.pkl")
-except:
-    print("Model not found")
+def get_model():
+    model = None
+    try:
+        model = joblib.load(current_path+"/Models/sentiment_analysis_naive_bayes_model.pkl")
+    except:
+        print("Model not found")
 
-if model!=None:
-    print(model.predict(data))
+    if model!=None:
+        print(model.predict(data))
