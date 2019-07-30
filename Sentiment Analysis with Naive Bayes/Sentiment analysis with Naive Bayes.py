fb0b0b2108e709f219a7891eb3a812477dec8d53
Sougata Bhattacharya <sougata.b@zohocorp.com>
2019-07-30 20:37:02 +0530

    moved to a new folder

---
+++ Sentiment Analysis with Naive Bayes/Sentiment analysis with Naive Bayes.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Load files
+
+# In[1]:
+
+import os
+current_path = os.getcwd()
+train = open(current_path+"/Data/amazon_reviews_train.csv","r+")
+test = open(current_path+"/Data/amazon_reviews_test.csv","r+")
+train = train.read()
+test = test.read()
+
+
+# In[ ]:
+
+
+
+
+
+# # Imports
+
+# In[8]:
+
+
+import numpy as np
+import pandas as pd
+import nltk
+import sklearn
+import string
+import textblob
+from sklearn.externals import joblib
+from nltk.stem.snowball import SnowballStemmer,PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.naive_bayes import GaussianNB,MultinomialNB
+
+
+# # Define necessary functions
+
+# In[3]:
+
+
+def messy_text_to_df(text):
+    documents = text.split("\n")
+    df = pd.DataFrame()
+    data = []
+    labels = []
+    for document in documents:
+        labels.append(document.split("\t",1)[0])
+        text = document.split('\t')[1]
+        data.append(text)
+    labels = np.array(labels)
+    labels[np.where(labels=='__label__2')] = "Positive"
+    labels[np.where(labels=='__label__1')] = "Negative"
+    df["Data"] = data
+    df["Label"] = labels
+
+    return df
+
+def remove_punctuation_and_numbers(text,replacements):
+    for key,value in replacements.items():
+        text = text.replace(key,value)
+    text = text.translate(str.maketrans('','',';"#$%&\'()*+/<=>?@[\\]^_`{|}~0123456789')).translate(str.maketrans('!.-:,','     '))
+    return text
+def remove_non_words(data,replacements):
+    res = data.apply(lambda x: remove_punctuation_and_numbers(x,replacements))
+    return res
+
+
+def remove_words_single(string,words_to_be_removed):
+    words = nltk.word_tokenize(string)
+    filtered_words = []
+    for i in range(len(words)):
+        if words[i] not in words_to_be_removed:
+            filtered_words.append(words[i])
+    return ' '.join(filtered_words)
+
+def remove_words(data,words_to_be_removed):
+    res = data.apply(lambda x : remove_words_single(x,words_to_be_removed))
+    return res
+
+    for text,label in documents:
+        labels.append(document.split("\t",1)[0])
+        text = document.split('\t')[1]
+        for key,value in replacements.items():
+            text = text.replace(key,value)
+            text = text.translate(str.maketrans('','',';"#$%&\'()*+/<=>?@[\\]^_`{|}~0123456789')).translate(str.maketrans('!.-:,','     '))
+        words = nltk.word_tokenize(text)
+        filtered_words = []
+        for i in range(len(words)):
+            if words[i] not in words_to_be_removed:
+                filtered_words.append(stemmer.stem(words[i]))
+
+        res = ' '.join(filtered_words)
+        data.append(res)
+    labels = np.array(labels)
+    labels[np.where(labels=='__label__2')] = "Positive"
+    labels[np.where(labels=='__label__1')] = "Negative"
+    return data,labels
+
+def stem_single_string(string,nltkstemmer):
+    words = nltk.word_tokenize(string)
+    stemmed_list = []
+    for word in words:
+        stemmed_list.append(nltkstemmer.stem(word))
+    return ' '.join(stemmed_list)
+
+
+def stem(data):
+    stemmer = SnowballStemmer("english")
+    res = data.apply(lambda x : stem_single_string(x,stemmer))
+    return res
+
+def find_rare_words(data,max_frequency=4):
+
+    vectoriser = get_vectorizer(data)
+
+
+    temp = ' '.join(data)
+    frequencies = (nltk.FreqDist(nltk.word_tokenize(temp)))
+
+    fs = np.array(frequencies.most_common())
+    fs = pd.DataFrame(fs)
+    fs.columns = ["word","count"]
+    fs["freq"] = fs["count"].astype(int)
+    fs = fs.drop("count",axis=1)
+
+    rare_words = list(fs[fs["freq"]<=max_frequency]["word"])
+
+    return rare_words
+
+def get_vectorizer(data,vectorizer="CountVectorizer"):
+
+    if vectorizer == "TFIDF":
+        tfidf = TfidfVectorizer()
+        tfidf.fit(data)
+        print("TF-IDF Vectorizer")
+        return tfidf
+    cv = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+    cv.fit(data)
+    print("Count Vectorizer")
+    return cv
+
+def vectorize_data(data,vectorizer="CountVectorizer"):
+    from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+    if vectorizer == "TFIDF":
+        tfidf = TfidfVectorizer()
+        tfidf.fit(data)
+        print("TF-IDF Vectorizer")
+        return tfidf.transform(data).toarray()
+    cv = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+    cv.fit(data)
+    print("Count Vectorizer")
+    return cv.transform(data).toarray()
+
+
+# In[4]:
+
+
+def remove_symbols_stopwords_and_stem(data):
+    data = messy_text_to_df(data)
+    data["Data"] = remove_non_words(data["Data"],replacements)
+    data["Data"] = remove_words(data["Data"],stopwords)
+    data["Data"] = stem(data["Data"])
+
+    return data
+
+def predict(data,model):
+    return model.predict(vectoriser.transform(data).toarray())
+# # Preprocessing
+
+# In[5]:
+
+
+replacements = {"can't" : 'can not',"shan't":'shall not',"won't":'will not',"'ve" : " have", "'d" : " would", "'m" : " am", "'ll" : " will", "'s" : "", "n't" : " not","'re" : "are"}
+stopwords = set(nltk.corpus.stopwords.words('english'))
+stopwords = nltk.corpus.stopwords.words('english')
+stopwords.remove("not")
+stemmer = SnowballStemmer("english")
+stopwords = set(["can","could","would","have","go","went","zero","one","two","three","four","five","six","seven","eight","nine","ten"]) | set(stopwords)
+
+train = remove_symbols_stopwords_and_stem(train)
+test = remove_symbols_stopwords_and_stem(test)
+
+rare_words = find_rare_words(train["Data"])
+
+train["Data"] = remove_words(train["Data"],rare_words)
+test["Data"] = remove_words(test["Data"],rare_words)
+
+
+# In[ ]:
+
+
+
+
+
+# # Vectorizer
+
+# In[6]:
+
+
+vectoriser = get_vectorizer(train["Data"])
+
+
+# # Model Training
+
+# In[10]:
+
+
+model = MultinomialNB()
+model.fit(vectoriser.transform(train["Data"]).toarray(),train["Label"])
+test["Prediction"] = model.predict(vectoriser.transform(test["Data"]).toarray())
+
+# In[25]:
+
+
+joblib.dump(model,current_path+"/Models/sentiment_analysis_naive_bayes_model.pkl")
+
+
+# # Evaluation
+
+# In[13]:
+
+
+F1_Score = sklearn.metrics.f1_score(np.array(test["Label"])=="Positive",test["Prediction"]=="Positive")
+Accuracy = sklearn.metrics.accuracy_score(test["Label"],test["Prediction"])
+print("Model trained.\n","F1 Score : ",F1_Score,"\nAccuracy : ",Accuracy)
---
+++ Sentiment Analysis with Naive Bayes/train.py
@@ -0,0 +1,177 @@
+import os
+current_path = os.getcwd()
+train = open(current_path+"/Data/amazon_reviews_train.csv","r+")
+test = open(current_path+"/Data/amazon_reviews_test.csv","r+")
+train = train.read()
+test = test.read()
+
+
+# In[ ]:
+
+
+
+print("running")
+
+# # Imports
+
+# In[8]:
+
+
+import numpy as np
+import os
+import pandas as pd
+import nltk
+import sklearn
+import string
+import textblob
+from sklearn.externals import joblib
+from nltk.stem.snowball import SnowballStemmer,PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.naive_bayes import GaussianNB,MultinomialNB
+
+
+
+class NaiveBayes:
+
+
+    def __init__(self):
+        self.replacements = {"can't" : 'can not',"shan't":'shall not',"won't":'will not',"'ve" : " have", "'d" : " would", "'m" : " am", "'ll" : " will", "'s" : "", "n't" : " not","'re" : "are"}
+        self.stopwords = set(["can","could","would","have","go","went","zero","one","two","three","four","five","six","seven","eight","nine","ten"]) | set(nltk.corpus.stopwords.words("english"))
+        self.stemmer = SnowballStemmer("english")
+        
+    #defuseful
+    def amazon_reviews_text_to_df(self,text):
+        documents = text.split("\n")
+        df = pd.DataFrame()
+        data = []
+        labels = []
+        for document in documents:
+            labels.append(document.split("\t",1)[0])
+            text = document.split('\t')[1]
+            data.append(text)
+        labels = np.array(labels)
+        labels[np.where(labels=='__label__2')] = "Positive"
+        labels[np.where(labels=='__label__1')] = "Negative"
+        df["Data"] = data
+        df["Label"] = labels
+
+        return df
+
+    #defuseful
+    def remove_punctuation_and_numbers(self,text):
+        replacements = self.replacements
+        for key,value in replacements.items():
+            text = text.replace(key,value)
+        text = text.translate(str.maketrans('','',';"#$%&\'()*+/<=>?@[\\]^_`{|}~0123456789')).translate(str.maketrans('!.-:,','     '))
+        return text
+    #defuseful
+    def remove_non_words(self,data):
+        res = data.apply(lambda x: self.remove_punctuation_and_numbers(x))
+        return res
+    #defuseful
+    def remove_words_single(self,string,words_to_be_removed):
+        words = nltk.word_tokenize(string)
+        filtered_words = []
+        for i in range(len(words)):
+            if words[i] not in words_to_be_removed:
+                filtered_words.append(words[i])
+        return ' '.join(filtered_words)
+    #defuseful
+    def remove_words(self,data,words_to_be_removed):
+        res = data.apply(lambda x : self.remove_words_single(x,words_to_be_removed))
+        return res
+
+    #defuseful
+    def stem_single_string(self,string,nltkstemmer):
+        words = nltk.word_tokenize(string)
+        stemmed_list = []
+        for word in words:
+            stemmed_list.append(nltkstemmer.stem(word))
+        return ' '.join(stemmed_list)
+
+    #defuseful
+    def stem(self,data):
+        stemmer = SnowballStemmer("english")
+        res = data.apply(lambda x : self.stem_single_string(x,stemmer))
+        return res
+    #defuseful
+    def find_rare_words(self,data,max_frequency=4):
+
+        vectoriser = self.get_vectorizer(data)
+
+
+        temp = ' '.join(data)
+        frequencies = (nltk.FreqDist(nltk.word_tokenize(temp)))
+
+        fs = np.array(frequencies.most_common())
+        fs = pd.DataFrame(fs)
+        fs.columns = ["word","count"]
+        fs["freq"] = fs["count"].astype(int)
+        fs = fs.drop("count",axis=1)
+
+        rare_words = list(fs[fs["freq"]<=max_frequency]["word"])
+
+        return rare_words
+    #defuseful
+    def get_vectorizer(self,data,vectorizer="CountVectorizer"):
+
+        if vectorizer == "TFIDF":
+            tfidf = TfidfVectorizer()
+            tfidf.fit(data)
+            print("Using TF-IDF Vectorizer")
+            return tfidf
+        cv = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+        cv.fit(data)
+        print("Using Count Vectorizer")
+        return cv
+    #defuseful
+    def vectorize_data(self,data,vectorizer="CountVectorizer"):
+
+        if vectorizer == "TFIDF":
+            tfidf = TfidfVectorizer()
+            tfidf.fit(data)
+            print("TF-IDF Vectorizer")
+            return tfidf.transform(data).toarray()
+        cv = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+        cv.fit(data)
+        print("Count Vectorizer")
+        return cv.transform(data).toarray()
+
+    #defuseful
+    def remove_symbols_stopwords_and_stem(self,data):
+        data = self.amazon_reviews_text_to_df(data)
+        data["Data"] = self.remove_non_words(data["Data"])
+        stopwords = self.stopwords
+        data["Data"] = self.remove_words(data["Data"],stopwords)
+        data["Data"] = self.stem(data["Data"])
+
+        return data
+
+    #defuseful
+    def train_amazon_model(self):
+        current_path = os.getcwd()
+        train = open(current_path+"/Data/amazon_reviews_train.csv","r+")
+        test = open(current_path+"/Data/amazon_reviews_test.csv","r+")
+        train = train.read()
+        test = test.read()
+
+        train = self.remove_symbols_stopwords_and_stem(train)
+        test = self.remove_symbols_stopwords_and_stem(test)
+
+        rare_words = self.find_rare_words(train["Data"])
+
+        train["Data"] = self.remove_words(train["Data"],rare_words)
+        test["Data"] = self.remove_words(test["Data"],rare_words)
+
+        vectoriser = self.get_vectorizer(train["Data"])
+
+        model = MultinomialNB()
+        model.fit(vectoriser.transform(train["Data"]).toarray(),train["Label"])
+        test["Prediction"] = model.predict(vectoriser.transform(test["Data"]).toarray())
+
+        joblib.dump(model,current_path+"/Models/sentiment_analysis_naive_bayes_model.pkl")
+        joblib.dump(vectoriser,current_path+"/Models/vectoriser.pkl")
+
+        F1_Score = sklearn.metrics.f1_score(np.array(test["Label"])=="Positive",test["Prediction"]=="Positive")
+        Accuracy = sklearn.metrics.accuracy_score(test["Label"],test["Prediction"])
+        print("Model trained.\n","F1 Score : ",F1_Score,"\nAccuracy : ",Accuracy)
